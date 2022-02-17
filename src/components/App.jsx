5479ee71198202d62999dfdf919384866e3828d3
mike591 <mikemach123@gmail.com>
2022-02-17 00:58:33 -0800

    more progress

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,54 +1,69 @@
-import { useEffect, useRef, useState, useCallback } from "react";
+import { useRef, useState } from "react";
+import Button from "@mui/material/Button";
+import UndoIcon from "@mui/icons-material/Undo";
+import RedoIcon from "@mui/icons-material/Redo";
+import CreateIcon from "@mui/icons-material/Create";
+import FormatColorResetIcon from "@mui/icons-material/FormatColorReset";
+
 import "./App.scss";
-import CanvasDraw from "react-canvas-draw";
+import { ReactSketchCanvas } from "react-sketch-canvas";
 import awkwardGuestsImg from "assets/awkward_guests_tracking_sheet.png";
 
 const App = () => {
-  const canvasContainerRef = useRef();
-  const [dimensions, setDimensions] = useState();
+  const canvasRef = useRef();
+  const [isErasing, setIsErasing] = useState(false);
 
-  const handleResize = useCallback(() => {
-    const aspectRatio = 1654 / 2339; // (h/w)
+  const handleUndo = () => {
+    canvasRef.current.undo();
+  };
 
-    let width = canvasContainerRef.current.offsetWidth;
-    let height = aspectRatio * width;
+  const handleRedo = () => {
+    canvasRef.current.redo();
+  };
 
-    while (height >= canvasContainerRef.current.offsetHeight) {
-      width *= 0.99;
-      height *= 0.99;
-    }
+  const setPencilMode = () => {
+    canvasRef.current.eraseMode(false);
+    setIsErasing(false);
+  };
 
-    setDimensions({
-      width: width,
-      height: height,
-    });
-  }, []);
+  const setEraseMode = () => {
+    canvasRef.current.eraseMode(true);
+    setIsErasing(true);
+  };
 
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    handleResize();
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, [handleResize]);
+  window.canvasRef = canvasRef.current;
 
   return (
     <div className="App">
       <div className="_menu"></div>
-      <div className="_container" ref={canvasContainerRef}>
-        {dimensions && (
-          <CanvasDraw
-            className="_canvas"
-            enablePanAndZoom
-            imgSrc={awkwardGuestsImg}
-            canvasWidth={dimensions.width}
-            canvasHeight={dimensions.height}
-            brushRadius={0}
-            lazyRadius={0}
-            hideInterface
-            zoomExtents={{ min: 0.5, max: 6 }}
-          />
-        )}
+      <div className="_container">
+        <div className="_controls">
+          <Button variant="outlined" onClick={handleUndo}>
+            <UndoIcon />
+          </Button>
+          <Button variant="outlined" onClick={handleRedo}>
+            <RedoIcon />
+          </Button>
+          <Button
+            variant={isErasing ? "outlined" : "contained"}
+            onClick={setPencilMode}
+          >
+            <CreateIcon />
+          </Button>
+          <Button
+            variant={isErasing ? "contained" : "outlined"}
+            onClick={setEraseMode}
+          >
+            <FormatColorResetIcon />
+          </Button>
+        </div>
+        <ReactSketchCanvas
+          ref={canvasRef}
+          strokeWidth={1}
+          strokeColor="black"
+          backgroundImage={awkwardGuestsImg}
+          preserveBackgroundImageAspectRatio="xMidYMid meet"
+        />
       </div>
     </div>
   );
